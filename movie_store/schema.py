80a0ba31916ae9577649712ac8cf7eb6c5589c8d
Victor Kiprop <kemboivictor@outlook.com>
2020-06-15 16:46:13 +0300

    added athentication and changed movies query resolvers

--- movie_store/schema.py
+++ movie_store/schema.py
@@ -1,14 +1,20 @@
 import graphene
 import movies.schema
 import market.schema
+import graphql_jwt
+
 class Query(movies.schema.Query,market.schema.Query ,graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 class Mutation(movies.schema.Mutation,market.schema.Mutation , graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
